6e93934784a86fba8c1e1d760ea8a9bf0b977b1a
f4ww4z <fawwazyusran@gmail.com>
2025-03-06 23:55:03 +0800

    Add tensorflow

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ from setuptools import setup, find_packages
 setup(
     name='malaya-speech-cli',
     version='0.1.0',
-    author='Your Name',
-    author_email='your.email@example.com',
+    author='f4ww4z',
+    author_email='fawwazyusran@gmail.com',
     description='A CLI application for generating audio files from text using Malaya Speech TTS VITS multispeaker model.',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=[
         'malaya-speech',
+        'pyaudio',
         'numpy',
-        'matplotlib',
-        'IPython'
+        'tensorflow[and-cuda]'
     ],
     entry_points={
         'console_scripts': [
